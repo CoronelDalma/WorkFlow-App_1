5254f3af9c3583bf47f3fd2a39805504b539afb8
Gisse88 <coronel.dg@gmail.com>
2022-03-27 20:24:15 -0300

    borrando repetidos

--- workflow/src/components/Modal/EditListModal.jsx
+++ workflow/src/components/Modal/EditListModal.jsx
@@ -6,9 +6,9 @@ const EditListModal = ({setModalOpen,action, data,idList}) => {
   return (
     <div>
         <div className='absolute left-0 top-0 h-screen w-screen bg-black bg-opacity-30 ' onClick={()=>{setModalOpen(false)}}></div>
-        <div className="bg-white w-2/4 text-color-text-h absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 rounded-lg text-color-text-h z-50">
+        <div className="bg-white w-2/4  absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2 rounded-lg text-color-text-h z-50">
             <Button className='absolute right-5 top-5 px-2'
-                onClick={()=>{setModalOpen(false)}}><FaWindowClose className='w-8 h-8 text-lavender-800 hover:text-lavender-600'/></Button>
+                onClick={()=>{setModalOpen(false)}}><FaWindowClose className='w-8 h-8 '/></Button>
             <h2 className='p-5 text-3xl font-bold'>Valor a cambiar</h2>
             <form className='flex flex-col p-5' onSubmit={action}>
                 <input name="description"  cols="30" rows="10"placeholder={data}></input>
