e990f235229373e0afbfa5e94bd9ba38f28753c6
Gisse88 <coronel.dg@gmail.com>
2022-03-27 21:22:16 -0300

    header responsive

--- workflow/src/components/layouts/Header.jsx
+++ workflow/src/components/layouts/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     const user = useSelector((state)=>state.user)
     const dispatch = useDispatch()
 
-    const [openMenu, setOpenMenu] = useState(true);
+    const [openMenu, setOpenMenu] = useState(false);
     const signOut = () =>{
         dispatch(logout())
       }
@@ -18,12 +18,12 @@ const Header = () => {
       <nav className="bg-color-btn py-3 z-999">
         <div className='max-w-screen-xl mx-auto flex justify-between relative pr-6'>
           <Link to="/"><Logo className='w-44'/></Link>
-          <ul className={`${openMenu?"block":"hidden"} bg-blue-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 md:flex absolute right-0 top-7 md:static p-2 pl-10 md:p-0 md space-y-2 md:space-y-0`}>
+          <ul className={`${openMenu?"block bg-color-btn":"hidden"} rounded-md bg-clip-padding  md:flex absolute right-0 top-7 md:static p-2 pl-10 md:p-0 md space-y-2 md:space-y-0`}>
               <NavItem to="/" title={"Link"}/>
               <NavItem to="/" title={"Link"}/>
               <NavItem to="/" title={"Link"}/>
 
-              {user.logged?<li onClick={signOut}>Cerrar sesión</li>:<NavItem to="/login" title={"Iniciar sesión"}/>}
+              {user.logged?<li onClick={signOut} className='mx-3 cursor-pointer'>Cerrar sesión</li>:<NavItem to="/login" title={"Iniciar sesión"}/>}
               {user.logged&&<p className='mx-2 text-white'>{user.name}-{user.role}</p>}
           </ul>
           <button onClick={()=>{setOpenMenu(!openMenu)}} className='block md:hidden mr-2 md:mr-0'>{openMenu?<FiXCircle className='h-5 w-5'/>:<FiMenu className='h-5 w-5'/>}</button>
